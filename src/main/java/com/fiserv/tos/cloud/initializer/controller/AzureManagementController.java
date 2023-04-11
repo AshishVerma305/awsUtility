cc3a1cdf97602b517f7606272d5bf19904580b05
F8MQPS0 <India@1234>
2023-04-11 23:39:01 +0530

    utility code

--- src/main/java/com/fiserv/tos/cloud/initializer/controller/AzureManagementController.java
+++ src/main/java/com/fiserv/tos/cloud/initializer/controller/AzureManagementController.java
@@ -6,21 +6,15 @@ import com.fiserv.tos.cloud.initializer.model.ErrorMessage;
 import com.fiserv.tos.cloud.initializer.model.InitializrResourceGroup;
 import com.fiserv.tos.cloud.initializer.model.InitializrSubscription;
 import com.fiserv.tos.cloud.initializer.model.InitializrVnet;
-import com.fiserv.tos.cloud.initializer.service.CloudInitalizrService;
 import com.fiserv.tos.cloud.initializer.util.AzureSecurityUtil;
 import com.microsoft.azure.management.Azure;
 import com.microsoft.azure.management.network.Network;
 import com.microsoft.azure.management.resources.ResourceGroup;
 import com.microsoft.azure.management.resources.Subscription;
 import lombok.AllArgsConstructor;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fiserv.tos.cloud.initializer.AzureUtilitiesApplication;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,9 +24,6 @@ import java.util.Collections;
 public class AzureManagementController {
 
     private AzureSecurityUtil azureSecurityUtil;
-
-    @Autowired
-    private CloudInitalizrService cloudInitalizrService;
 
     @GetMapping("/subscriptions")
     public String getSubscriptions() {
@@ -88,14 +79,6 @@ public class AzureManagementController {
         return jsonMessage(list);
     }
 
-    @GetMapping("/getApplications")
-    public String getApplications(@RequestParam(name = "workspaceid") String workspaceid, @RequestParam(name = "userid") String userid) {
-        try {
-            return  cloudInitalizrService.getApplications(workspaceid, userid);
-        } catch (Exception e) {
-            return jsonMessage(new ErrorMessage("Error accessing subscription:"+e.getMessage()));
-        }
-    }
 
 
     private static String jsonMessage(Object error){
--- src/main/java/com/fiserv/tos/cloud/initializer/service/CloudInitalizrService.java
+++
@@ -1,9 +0,0 @@
-package com.fiserv.tos.cloud.initializer.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-
-public interface CloudInitalizrService {
-     String getApplications(String workspaceID, String userId) throws JsonProcessingException;
-}
-
