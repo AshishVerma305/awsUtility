ee104117be4d929a69b71ba60c64dc7301f7db04
F8MQPS0 <India@1234>
2023-04-21 10:23:22 +0530

    getAllResources Api

--- src/main/java/com/fiserv/tos/cloud/initializer/controller/AzureManagementController.java
+++ src/main/java/com/fiserv/tos/cloud/initializer/controller/AzureManagementController.java
@@ -13,6 +13,7 @@ import com.microsoft.azure.management.resources.Subscription;
 
 import com.microsoft.azure.management.resources.fluentcore.arm.ResourceId;
 import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,7 +73,6 @@ public class AzureManagementController {
         } catch (Exception e) {
             return jsonMessage(new ErrorMessage("Error accessing subscription:"+e.getMessage()));
         }
-        // Get the list of subscriptions
         ArrayList<InitializrResourceGroup> list = new ArrayList<InitializrResourceGroup>();
         for (ResourceGroup resourceGroup : azure.resourceGroups().list()){
             list.add(new InitializrResourceGroup(resourceGroup.name(), resourceGroup.tags()));
@@ -144,7 +144,48 @@ public class AzureManagementController {
         }
         return jsonMessage(resourceList);
     }
+    @GetMapping("/getVms")
+    public String getAllVMS(@RequestParam(name = "subscriptionid") String subscriptionId, @RequestParam(name = "resourceGroupName") String resourceGroupName, Model model) {
+        Azure azure = null;
+        try {
+            azure = this.azureSecurityUtil.getAzure(subscriptionId);
+        } catch (Exception e) {
+            return jsonMessage(new ErrorMessage("Error accessing subscription:"+e.getMessage()));
+        }
 
+        ResourceGroup resourceGroup = azure.resourceGroups().getByName(resourceGroupName);
+        if (resourceGroup == null) {
+            System.out.printf("Resource group '%s' not found.%n", resourceGroupName);
+            return "x";
+        }
+
+        System.out.printf("Resource group '%s' found with ID: %s%n", resourceGroupName, resourceGroup.id());
+
+        List<GenericResource> resources = azure.genericResources().listByResourceGroup(resourceGroupName);
+        List resourceList=new ArrayList();
+        if (resources == null || resources.isEmpty()) {
+            System.out.printf("No resources found in resource group '%s'.%n", resourceGroupName);
+        } else {
+            System.out.printf("Resources in resource group '%s':%n", resourceGroupName);
+            for (GenericResource resource : resources) {
+                ResourceId resourceId = ResourceId.fromString(resource.id());
+                System.out.println(resourceId);
+                if(resourceId.resourceType().equalsIgnoreCase("virtualMachines"))
+                {
+                    Resources resourcesData=new Resources();
+                    resourcesData.setResourceId(resourceId.toString());
+                    resourcesData.setResourceName(resourceId.name());
+                    resourcesData.setResourceType(resourceId.resourceType());
+                    resourceList.add(resourcesData);
+                    model.addAttribute("subscriptionId",subscriptionId);
+                    model.addAttribute("resourceGroupName",resourceGroupName);
+                    model.addAttribute("resource",resourceId.name());
+                }
+                System.out.printf("- Resource type: %s, Name: %s, ID: %s%n", resourceId.resourceType(), resourceId.name(), resourceId.toString());
+            }
+        }
+        return "chaosAzureVmYaml";
+    }
 
     private static String jsonMessage(Object error){
         ObjectMapper mapper = new ObjectMapper();
